4297031b571c40c271d13dc90ae5edcb2c344ae4
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-12-20 15:37:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,12 +47,14 @@ resource "aws_s3_bucket_policy" "public_read" {
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key    = "index.html"
+  source = "index.html"
   acl    = "public-read"
   content_type = "text/html"
   }
 resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.mybucket.id
   key    = "error.html"
+  source = "error.html"
   acl    = "public-read"
   content_type = "text/html"
   }
